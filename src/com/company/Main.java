a8e59fde6fe63a5c38f1f98de205be3260e0fe48
Ivan Valentinov <ivan156drs@gmail.com>
2021-03-17 08:30:44 +0200

    Updated files with functions.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,37 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	    Test1();
+    public static void main(String[] args) throws CloneNotSupportedException {
+//     Test1();
+//        Test2();
+//        Test3();
+        Test4();
+    }
+
+    public static void Test4() throws CloneNotSupportedException {
+        Polynomial a = Polynomial.create();
+        Polynomial b = Polynomial.create();
+        Polynomial c = Polynomial.multiplyPolynomialPolynomial(a, b);
+        System.out.println(Polynomial.toString(c));
+    }
+
+    public static void Test3(){
+        Scanner in = new Scanner(System.in);
+        Polynomial a = Polynomial.create();
+        System.out.print("Coefficient and power of the monomial: ");
+        Monomial b = new Monomial(in.nextDouble(), in.nextInt());
+        Polynomial c = Polynomial.multiplyPolynomialMonomial(a, b);
+        System.out.println(Polynomial.toString(c));
+    }
+
+    public static void Test2() throws CloneNotSupportedException {
+        Polynomial a = Polynomial.create();
+        Polynomial b = Polynomial.create();
+        Polynomial c = Polynomial.add(a, b);
+        System.out.println(Polynomial.toString(c));
     }
 
     public static void Test1(){
